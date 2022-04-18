564e820ade8c456185011e4efbf6ce4ba371f9f5
g-utsav34 <g.utsav34@gmail.com>
2022-04-18 13:01:01 +0530

    added

---
+++ dynamic Programming/climbStairs/main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn= new Scanner(System.in);
+
+        int N=scn.nextInt();
+        int[] dp=new int[N+1];
+        dp[0]=1;
+        for(int n=1;n<=N;n++){
+            for(int i=1;i<=3&&n-i>=0;i++){
+                dp[n]+=dp[n-i];
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+
+    
+
+}
