b45fdea7a95f7dcc0b0b213d64f0a7a0f4fe0c40
g-utsav34 <g.utsav34@gmail.com>
2022-04-23 20:28:45 +0530

    added

---
+++ dynamic Programming and greedy/tilingFloor/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+/* -------------------------------------------------------------------------- */
+/*                       tiling 2xn floor with 2x1 tiles                      */
+/* -------------------------------------------------------------------------- */
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int ans=tiling_tab(n);
+    int ans=tiling_recur(n);
+    System.out.println(dp[n]);
+
+  }
+/* -------------------------------- Recursive ------------------------------- */
+  public static int tiling(int n) {
+    if (n == 1)
+      return 1;
+      
+    if(n==2)
+        return 2;
+        
+    int count=tiling(n-1)+tiling(n-2);
+    
+    return count;
+  }
+/* ------------------------------- Tabulation ------------------------------- */
+  public static int tiling_tab(int n){
+      int dp[]= new int[n+1];
+    dp[1]=1;
+    dp[2]=2;
+    for(int i=3;i<=n;i++)
+    dp[i]=dp[i-1]+dp[i-2];
+
+    return (dp[n]);
+  }
+}
